cc9d1728e7257bc28da769eeb95763ec682d6771
Eon Maxell Busque <165742990+23-00175@users.noreply.github.com>
2024-12-14 13:03:53 +0800

    Update TrackerDB.java

--- src/tracker/TrackerDB.java
+++ src/tracker/TrackerDB.java
@@ -2,7 +2,6 @@ package tracker;
 
 import java.sql.Connection;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import main.WellnessDB;
@@ -20,22 +19,7 @@ public abstract class TrackerDB {
         this.userId = userId;
         this.trackerType = trackerType;
         this.connection = WellnessDB.getConnection();
-        this.activityDate = (activityDate != null) ? activityDate : promptForDate();
-    }
-
-    // Method to prompt for date input
-    public LocalDate promptForDate() {
-        while (true) {
-            System.out.print("Enter the activity date (YYYY-MM-DD): ");
-            String dateInput = scanner.nextLine().trim();
-
-            try {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                return LocalDate.parse(dateInput, formatter);
-            } catch (Exception e) {
-                System.out.println("Invalid date format. Please enter the date in the format YYYY-MM-DD.");
-            }
-        }
+        this.activityDate = activityDate;
     }
 
     // Getters
